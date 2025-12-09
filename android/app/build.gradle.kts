e7fcc6e915660c5f69a32ba76d9ba85a7be01716
CharlesCraft50 <charlescraft50yt@gmail.com>
2025-12-09 23:20:11 +0800

    S

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.manong.app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = flutter.compileSdkVersion.toInt()
     ndkVersion = "27.0.12077973" // flutter.ndkVersion
 
     compileOptions {
@@ -36,9 +36,9 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        targetSdk = flutter.targetSdkVersion.toInt()
+        versionCode = (flutter.versionCode?.toIntOrNull() ?: 1)
+        versionName = (flutter.versionName ?: "1.0.0")
     }
 
     signingConfigs {
